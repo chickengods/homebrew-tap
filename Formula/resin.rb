ec55df1afa0b34492ce7abef81d6cf27a5805535
Adam Aguilera <adamkaguilera@gmail.com>
2026-07-17 17:45:29 -0400

    resin 0.2.0
    Bump formula to v0.2.0 (source chickengods/resin @ 67ac12e).
    Prebuilt darwin arm64/amd64 artifacts; unsigned prerelease.

--- Formula/resin.rb
+++ Formula/resin.rb
@@ -1,19 +1,19 @@
 class Resin < Formula
   desc "Context layer that ends your agent's cold starts"
   homepage "https://resin.fyi"
-  version "0.1.3"
+  version "0.2.0"
 
   depends_on :macos
 
   on_macos do
     on_arm do
-      url "https://github.com/chickengods/homebrew-tap/releases/download/v0.1.3/resin_0.1.3_darwin_arm64.tar.gz"
-      sha256 "0155a15dfae0304663f749eecd691be7469d1a1349c87d343c3580a3ab7521bd"
+      url "https://github.com/chickengods/homebrew-tap/releases/download/v0.2.0/resin_0.2.0_darwin_arm64.tar.gz"
+      sha256 "9f2fb78f57a06c54a3b2469d4bce28d6605f3323747ba5a898cec8e8cb999b38"
     end
 
     on_intel do
-      url "https://github.com/chickengods/homebrew-tap/releases/download/v0.1.3/resin_0.1.3_darwin_amd64.tar.gz"
-      sha256 "b232838be433efd89f8199935426efb29d6291f5fd9df7388e5df5d8026fa127"
+      url "https://github.com/chickengods/homebrew-tap/releases/download/v0.2.0/resin_0.2.0_darwin_amd64.tar.gz"
+      sha256 "4008bb801042a5f8496999ea0c9f5ca10513d01e228db64c1e170941617c44c1"
     end
   end
 
@@ -23,7 +23,7 @@ class Resin < Formula
 
   test do
     output = shell_output("#{bin}/resin version --json")
-    assert_match '"version":"v0.1.3"', output
+    assert_match '"version":"v0.2.0"', output
     assert_match '"channel":"prod"', output
     assert_match '"distribution":"homebrew"', output
   end
